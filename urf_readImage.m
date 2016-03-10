6c7f461f250a7f3afe506be421bb36627fd154d6
felipepitoko <lip-sousa@hotmail.com>
2016-03-10 00:17:11 -0300

    Update urf_readImage.m

--- urf_readImage.m
+++ urf_readImage.m
@@ -1,9 +1,20 @@
 function X = read_image(filename)
-    % lê uma imagem e retorna um vetor representando a imagem (já convertida pra escala de cinza)
-    %
-    % espera:
-    % |filename| o nome da imagem pra ler
-    %
-    % retorna:
-    % |X| um vetor representando a imagem
+   function X = read_image(filename)
+        %Lê uma imagem inserida e a transforma em um vetor 
+	X = [];
+	try
+		X = double(imread(filename)); %Pega a imagem e transforma em números 
+		[height width channels] = size(X); %Usa a altura, largura e quantidade de cor como elementos do vetor
+		% se tiver 3 canais de cores (figurada colorida), transforma a figura em escala de cinza
+		if(channels == 3)
+			X = (X(:,:,1) + X(:,:,2) + X(:,:,3)) / 3;
+		end
+		X = reshape(X,width*height,1);
+
+        %captura um possível erro na recepção da imagem e mostra uma mensagem com o nome do erro    
+	catch 
+		lerr = lasterror; 
+		fprintf(1,'Cannot read image %s.\nReason:\n%s\n', filename, lerr);
+	end
+en
 end
